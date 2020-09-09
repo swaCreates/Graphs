1cb349c0c2da6ae33c905c85d0a29393b4e014a2
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-09-09 17:15:54 -0400

    working on adv.py

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -8,35 +8,9 @@ from ast import literal_eval
 # Load world
 world = World()
 
-class Queue():
-    def __init__(self):
-        self.queue = []
-    def enqueue(self, value):
-        self.queue.append(value)
-    def dequeue(self):
-        if self.size() > 0:
-            return self.queue.pop(0)
-        else:
-            return None
-    def size(self):
-        return len(self.queue)
-
-class Stack():
-    def __init__(self):
-        self.stack = []
-    def push(self, value):
-        self.stack.append(value)
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop()
-        else:
-            return None
-    def size(self):
-        return len(self.stack)
-
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "/Users/swa_isthecreator/Documents/Lambda-CS-Projects/Graphs/projects/adventure/maps/test_line.txt"
+# map_file = "/Users/swa_isthecreator/Documents/Lambda-CS-Projects/Graphs/projects/adventure/maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -52,29 +26,23 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = ['n', 's']
 
-room = player.current_room
+traversal_path = []
 
-s = Stack()
-s.push(room)
+curr_path = []
 
-room_dict = {}
-room_directions = {
-    'n': '?',
-    's': '?',
-    'e': '?',
-    'w': '?',
+rooms_visited = set()
+
+reverse_dir= {
+    'n': 's',
+    's': 'n',
+    'w': 'e',
+    'e': 'w'
 }
 
-while s.size() > 0:
-    curr_room = s.pop()
-    room_dict[0] = room_directions
-
-
-    # curr_obj = s.pop()
-    # curr_room = curr_obj['starting_room']
-    # curr_path = curr_obj['path']
+# curr_obj = s.pop()
+# curr_room = curr_obj['starting_room']
+# curr_path = curr_obj['path']
 
 # Create Traversal Graph / Adjacency List
 
@@ -86,8 +54,34 @@ while s.size() > 0:
 
 """You know you are done when you have exactly 500 entries (0-499) in your graph and no '?' in the adjacency dictionaries. To do this, you will need to write a traversal algorithm that logs the path into traversal_path as it walks."""
 
-# rooms should be stored as int (key) vertex
-# traversal direction should be stored as input (value) edges
+
+# U - UPER
+# DFS approach
+
+
+# 1. begin in current room
+curr_room = player.current_room
+print(f'These are the room exits:', curr_room.get_exits())
+# 1a Get all possible room exits
+# use room.getexits() ==== will return arr of n,s,e,w
+exits = curr_room.get_exits()
+# 2. choose to go in a random direction that hasn't been visited  
+for exit in exits:
+    curr_room.get_room_in_direction(exit)
+    print(f'These are my directions:', curr_room.get_room_in_direction(exit))
+# 3. move to direction / make sure our choice logs the direction chosen
+    path_choice = random.choice(exit)
+    traversal_path.append(path_choice)
+    # log the reverse direction of the path taken
+    reverse_path_choice = reverse_dir[path_choice]
+    curr_path.append(reverse_path_choice)
+# 4. make previous direction the room id
+# 5. repeat steps 1 - 3
+
+# BFS approach
+# 5. When we are in a room with all explored paths (ex. Nomore '?')
+# 6. We want to traverse backward and find any paths that have not been explored
+
 
 # DFS
 # push starting room onto stack
---
+++ projects/adventure/test.py
@@ -0,0 +1,5 @@
+from ast import literal_eval
+
+map__file = "/Users/swa_isthecreator/Documents/Lambda-CS-Projects/Graphs/projects/adventure/maps/test_line.txt"
+
+graph = literal_eval(open(map__file, "r").read())
