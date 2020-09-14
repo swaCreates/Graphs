72eb42aad27da07e8b303e8c4b617395ac54155b
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-09-14 18:55:47 -0400

    passing test_line world

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -9,8 +9,8 @@ from ast import literal_eval
 world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "/Users/swa_isthecreator/Documents/Lambda-CS-Projects/Graphs/projects/adventure/maps/test_line.txt"
-# map_file = "/Users/swa_isthecreator/Documents/Lambda-CS-Projects/Graphs/projects/adventure/maps/test_cross.txt"
+# map_file = "/Users/swa_isthecreator/Documents/Lambda-CS-Projects/Graphs/projects/adventure/maps/test_line.txt"
+map_file = "/Users/swa_isthecreator/Documents/Lambda-CS-Projects/Graphs/projects/adventure/maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -27,23 +27,6 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 
-traversal_path = []
-
-curr_path = []
-
-rooms_visited = set()
-
-reverse_dir= {
-    'n': 's',
-    's': 'n',
-    'w': 'e',
-    'e': 'w'
-}
-
-# curr_obj = s.pop()
-# curr_room = curr_obj['starting_room']
-# curr_path = curr_obj['path']
-
 # Create Traversal Graph / Adjacency List
 
 # what are my vertices? .....room
@@ -58,23 +41,55 @@ reverse_dir= {
 # U - UPER
 # DFS approach
 
+traversal_path = []
 
-# 1. begin in current room
-curr_room = player.current_room
-print(f'These are the room exits:', curr_room.get_exits())
-# 1a Get all possible room exits
-# use room.getexits() ==== will return arr of n,s,e,w
-exits = curr_room.get_exits()
-# 2. choose to go in a random direction that hasn't been visited  
-for exit in exits:
-    curr_room.get_room_in_direction(exit)
-    print(f'These are my directions:', curr_room.get_room_in_direction(exit))
-# 3. move to direction / make sure our choice logs the direction chosen
-    path_choice = random.choice(exit)
-    traversal_path.append(path_choice)
-    # log the reverse direction of the path taken
-    reverse_path_choice = reverse_dir[path_choice]
-    curr_path.append(reverse_path_choice)
+curr_path = []
+
+rooms_visited = set()
+
+reverse_dict= {
+    'n': 's',
+    's': 'n',
+    'w': 'e',
+    'e': 'w'
+}
+
+
+
+while len(rooms_visited) < len(world.rooms):
+
+    # 1. begin in current room
+    curr_room = player.current_room
+    print(f'These are the room exits:', curr_room.get_exits())
+    print(f'Current room id:', curr_room.id)
+    print(f'All rooms:', len(world.rooms))
+
+    # 1a Get all possible room exits
+    # use room.getexits() ==== will return arr of n,s,e,w
+    exits = curr_room.get_exits()
+
+    # 2. choose to go in a random direction that hasn't been visited  
+    for exit in exits:
+        curr_room.get_room_in_direction(exit)
+        print(f'These are my directions:', curr_room.get_room_in_direction(exit))
+
+    # 3. move to direction / make sure our choice logs the direction chosen
+        if curr_room.id not in rooms_visited:
+            # add the curr_room into visited
+            rooms_visited.add(curr_room.id)
+            print(rooms_visited)
+            
+            path_choice = random.choice(exit)
+            player.travel(path_choice)
+            traversal_path.append(path_choice)
+            print(f'My path:', traversal_path)
+            # log the reverse direction of the path taken
+            reverse_path_choice = reverse_dict[path_choice]
+            curr_path.append(reverse_path_choice)
+            # allow player to travel in reverse also
+            reverse_dir = reverse_path_choice[0]
+            # player.travel(reverse_dir)
+
 # 4. make previous direction the room id
 # 5. repeat steps 1 - 3
 
@@ -93,12 +108,6 @@ for exit in exits:
 # BFS
     # else if stuck
     # retrace steps until you can find an unexplored route
-
-
-# Still confused on how I would be using traversal_path
-# or
-# If I use the room_directions, how would I make changes to that or use that or should I make changes at all
-
 
 
 
